c1f1bec00309da5adff43f4978511c23ed2a75c2
Ranjodh Singh <r.singh@revolentgroup.com>
2022-01-21 13:18:35 -0500

    initial commit

---
+++ force-app/main/default/lwc/wiredOpportunities/wiredOpportunities.js
@@ -0,0 +1,15 @@
+import { LightningElement, track, wire } from 'lwc';
+import getStages from '@salesforce/apex/OpportunityController.getStages';
+import getOpportunity from '@salesforce/apex/OpportunityController.getOpportunity';
+export default class WiredOpportunities extends LightningElement {
+    @track currentOpportunity = '';
+    @wire(getStages) stageNames;
+    @wire(getOpportunity, { stgName: '$currentOpportunity' }) OpportunityDetails;
+    columns = [    
+        { label: 'Label', fieldName: 'Name' },
+        { label: 'Stage', fieldName: 'StageName' }
+        ]   
+    handleChange = (eve) => {
+        this.currentOpportunity = eve.target.value;
+    }
+}
