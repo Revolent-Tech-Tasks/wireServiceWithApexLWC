65e7f9fee5822e666b7f07b3a8dd25e01d7477c8
Ranjodh Singh <r.singh@revolentgroup.com>
2022-01-24 19:27:59 -0500

    testing with data

--- force-app/main/default/lwc/wiredOpportunities/__tests__/wiredOpportunities.test.js
+++ force-app/main/default/lwc/wiredOpportunities/__tests__/wiredOpportunities.test.js
@@ -1,12 +1,12 @@
 import { createElement } from 'lwc';
 import WiredOpportunities from 'c/wiredOpportunities';
-import { registerApexTestWireAdapter } from '@salesforce/sfdx-lwc-jest';
+//import { registerApexTestWireAdapter } from '@salesforce/sfdx-lwc-jest';
 import getStages from '@salesforce/apex/OpportunityController.getStages';
 // Realistic data with a list of contacts
 const mockGetStages = require('./data/stageList.json');
 
 // Register as Apex wire adapter. Some tests verify that provisioned values trigger desired behavior.
-const getStagesListAdapter = registerApexTestWireAdapter(getStages);
+//const getStagesListAdapter = registerApexTestWireAdapter(getStages);
 
 describe('c-wired-opportunities', () => {
     afterEach(() => {
@@ -19,17 +19,33 @@ describe('c-wired-opportunities', () => {
     });
 
     describe("getStages @wire data",()=>{
+        it('gets the lightning combobox', () => {
+            const element = createElement('c-wired-opportunities', {
+                is: WiredOpportunities
+            });
+            document.body.appendChild(element);
+            return Promise.resolve().then(()=>{
+                // Select elements for validation
+                const oppElement = element.shadowRoot.querySelector('lightning-combobox');    
+                expect(oppElement.name).toBe("stage combo box");
+            });
+        });
+    });
+
+    describe("test for stage names",()=>{
         it('Renders the stageName list in the dropdown element', () => {
             const element = createElement('c-wired-opportunities', {
                 is: WiredOpportunities
             });
             document.body.appendChild(element);
-            getStagesListAdapter.emit(mockGetStages);
+            // getStagesListAdapter.emit(mockGetStages);
+            getStages.emit(mockGetStages);
             
             return Promise.resolve().then(()=>{
                 // Select elements for validation
-                const oppElement = element.shadowRoot.querySelector('lightning-combobox');    
-                expect(oppElement.name).toBe("stage combo box");
+                const oppElement = element.shadowRoot.querySelector('lightning-combobox');
+                // const stageListLength = mockGetStages.stageNames.length();
+                expect(oppElement.length).toBe(11);
             });
         });
     });
